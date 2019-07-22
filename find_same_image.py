e8a60424a2d8ea88d3230ca489f2e08bcb97d0d0
C.TheFreeLife <43929311+TheFreeLife@users.noreply.github.com>
2019-07-22 15:38:30 +0900

    Update find_same_image.py

--- find_same_image.py
+++ find_same_image.py
@@ -1,7 +1,7 @@
 from PIL import Image 
 
-im = Image.open("C:/Users/happy/Desktop/test_folder/kei100.jpeg") 
-im2 = Image.open("C:/Users/happy/Desktop/test_folder/kei300.jpeg")
+im = Image.open("test_image5.jpeg") 
+im2 = Image.open("test_image10.jpeg")
 
 pixels = list(im.getdata())
 pixels2 = list(im2.getdata())
